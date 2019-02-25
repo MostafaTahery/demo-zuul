cade827043f1d00346b406ab7460b88e55d3123c
Mostafa Tahery <smt4940@gmail.com>
2019-02-25 15:10:25 +0330

    0.01 rest Template

--- src/main/java/co/nilin/spring/cloud/apigateway/demozuul/DemoZuulApplication.java
+++ src/main/java/co/nilin/spring/cloud/apigateway/demozuul/DemoZuulApplication.java
@@ -4,15 +4,25 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 
 @EnableZuulProxy
 @EnableDiscoveryClient
 @SpringBootApplication
+@RestController
 public class DemoZuulApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoZuulApplication.class, args);
 	}
 
+	@GetMapping("/my-health-check")
+	public ResponseEntity<String> myCustomCheck(){
+		String message="Testing My Health Function";
+		return new ResponseEntity<>(message, HttpStatus.OK);
+	}
 }
