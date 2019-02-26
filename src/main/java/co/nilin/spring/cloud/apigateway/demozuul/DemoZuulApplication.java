d57215bbd3db6a83ab9700c38c7f95c26c10a34a
Mostafa Tahery <smt4940@gmail.com>
2019-02-26 13:41:44 +0330

    0.02 rest Template - feign Client

--- src/main/java/co/nilin/spring/cloud/apigateway/demozuul/DemoZuulApplication.java
+++ src/main/java/co/nilin/spring/cloud/apigateway/demozuul/DemoZuulApplication.java
@@ -4,16 +4,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
 @EnableZuulProxy
 @EnableDiscoveryClient
 @SpringBootApplication
 @RestController
+@EnableFeignClients
 public class DemoZuulApplication {
 
 	public static void main(String[] args) {
